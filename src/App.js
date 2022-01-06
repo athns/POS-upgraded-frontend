ab4d05aab6483ae4ec9a9e916a95785138a14cce
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2022-01-06 14:41:37 -0600

    Finished debugging console on MainMenu, Starting Blog Merge

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
         <Routes>
          <Route path="/" element={<LandingPage />}/>
          <Route path="/MainMenu" element={<MenuPage />}/>
-      
+        
          </Routes>
        </div>
      </Router>
