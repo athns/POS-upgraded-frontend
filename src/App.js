a3709ea00e9c721dd227a06bbc5881b35b899622
Bloo00 <kabin.yangg@gmail.com>
2022-01-06 06:57:47 -0800

    added the route lol forgot about that

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 import MenuPage from "./components/MenuPage";
 import LandingPage from "./components/LandingPage";
 import Ghost from "./components/Ghost"
+import OptionsContainer from "./components/OptionsContainer";
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
          <Route path="/" element={<LandingPage />}/>
          <Route path="/MainMenu" element={<MenuPage />}/>
          <Route path="/ghost" element={<Ghost />}/>
+         <Route path="/options" element={<OptionsContainer />}/>
          </Routes>
        </div>
      </Router>
