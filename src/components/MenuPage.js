82ad2f8d2c32fc6d92ed06f9c6f852dde3ae9817
Mekel Tensley <mekelintech@gmail.com>
2022-01-06 19:09:38 -0500

    Create buttons, add title and restaurant image

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -9,75 +9,45 @@ class MenuPage extends Component {
     render() {
         return (
             <>
+                <div className="main-navbar">
+                    <div>
+                        <h1 className='restaurant-name'>Restaurant Name</h1>
+                    </div>
                     <div class="navbar-item">
-                        <div class="field is-grouped">
-                            <p class="control">
-                                <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="#" target="_blank" href="https://twitter.com/intent/tweet?text=get free bulma templates:;url=https://github.com/BulmaTemplates/bulma-templates">
-                                    <span class="icon">
-                                        <i class="fab fa-twitter"></i>
-                                    </span>
-                                    <span>
-                                        Tweet
-                                    </span>
-                                </a>
+                        <div class="order-buttons">
+                            <p class="seat-number">
+                                <span>
+                                    Seat Number :
+                                </span>
                             </p>
-                            <p class="control">
-                                <a class="button is-primary" href="https://github.com/BulmaTemplates/bulma-templates">
-                                    <span class="icon">
-                                        <i class="fas fa-download"></i>
-                                    </span>
-                                    <span>Download</span>
-                                </a>
+                            <p class="order-total">
+                                <span>
+                                    Order Total : $
+                                </span>
                             </p>
                         </div>
                     </div>
-                <section class="hero is-info">
-                    <div class="hero-body">
-                        <div class="container">
-                            <h1 class="title">
-                                Tabs
-                            </h1>
-                            <h2 class="subtitle">
-                                example
-                            </h2>
-                        </div>
-                    </div>
+                </div>
+                <section class="main-image">
+                    <div class="hero-body"></div>
                     <div class="tabs is-boxed is-centered main-menu" id="nav">
                         <ul>
                             <li data-target="pane-1" id="1">
-                                <a>
-                                    <span class="icon is-small"><i class="fa fa-image"></i></span>
-                                    <span>Pictures</span>
+                                <a href='#'>
+                                    <span>Drink</span>
                                 </a>
                             </li>
                             <li data-target="pane-2" id="2" class="is-active">
-                                <a>
-                                    <span class="icon is-small"><i class="fab fa-empire"></i></span>
-                                    <span>Article</span>
+                                <a href='#'>
+                                    <span>Eat</span>
                                 </a>
                             </li>
                             <li data-target="pane-3" id="3">
-                                <a>
-                                    <span class="icon is-small"><i class="fab fa-superpowers"></i></span>
-                                    <span>Team</span>
-                                </a>
-                            </li>
-                            <li data-target="pane-4" id="4">
-                                <a>
-                                    <span class="icon is-small"><i class="fa fa-film"></i></span>
-                                    <span>Video</span>
+                                <a href='#'>
+                                    <span>Desert</span>
                                 </a>
                             </li>
                         </ul>
-                    </div>
-                    <div class="tab-content"></div>
-                    <div class="tab-pane" id="pane-1">
-                        <figure>
-                            <img src="https://source.unsplash.com/0_xMuEbpFAQ/400x400" alt="💯" class="cent"></img>
-                        </figure>
-                        <figure>
-                            <img src="https://source.unsplash.com/wPMvPMD9KBI/800x600" alt="💯" class="cent"></img>
-                        </figure>
                     </div>
                     <div class="tab-pane" id="pane-3">
                         <div class="columns">
@@ -141,99 +111,6 @@ class MenuPage extends Component {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane" id="pane-4">
-                        <div class="columns is-centered">
-                            <div class="column is-three-quarters">
-                                <div class="embed-container image">
-                                    <iframe src="https://player.vimeo.com/video/261794608" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane is-active" id="pane-2">
-                        <div class="content">
-                            <h1>Hello World</h1>
-                            <p>Lorem ipsum<sup><a>[1]</a></sup> dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque. Sub<sub>script</sub> works as well!</p>
-                            <h2>Second level</h2>
-                            <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
-                            <ul>
-                                <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
-                                <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
-                                <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
-                                <li>Ut non enim metus.</li>
-                            </ul>
-                            <h3>Third level</h3>
-                            <p>Quisque ante lacus, malesuada ac auctor vitae, congue <a href="#">non ante</a>. Phasellus lacus ex, semper ac tortor nec, fringilla condimentum orci. Fusce eu rutrum tellus.</p>
-                            <ol>
-                                <li>Donec blandit a lorem id convallis.</li>
-                                <li>Cras gravida arcu at diam gravida gravida.</li>
-                                <li>Integer in volutpat libero.</li>
-                                <li>Donec a diam tellus.</li>
-                                <li>Aenean nec tortor orci.</li>
-                                <li>Quisque aliquam cursus urna, non bibendum massa viverra eget.</li>
-                                <li>Vivamus maximus ultricies pulvinar.</li>
-                            </ol>
-                            <blockquote>Ut venenatis, nisl scelerisque sollicitudin fermentum, quam libero hendrerit ipsum, ut blandit est tellus sit amet turpis.</blockquote>
-                            <p>Quisque at semper enim, eu hendrerit odio. Etiam auctor nisl et <em>justo sodales</em> elementum. Maecenas ultrices lacus quis neque consectetur, et lobortis nisi molestie.</p>
-                            <p>Sed sagittis enim ac tortor maximus rutrum. Nulla facilisi. Donec mattis vulputate risus in luctus. Maecenas vestibulum interdum commodo.</p>
-                            <dl>
-                                <dt>Web</dt>
-                                <dd>The part of the Internet that contains websites and web pages</dd>
-                                <dt>HTML</dt>
-                                <dd>A markup language for creating web pages</dd>
-                                <dt>CSS</dt>
-                                <dd>A technology to make HTML look better</dd>
-                            </dl>
-                            <p>Suspendisse egestas sapien non felis placerat elementum. Morbi tortor nisl, suscipit sed mi sit amet, mollis malesuada nulla. Nulla facilisi. Nullam ac erat ante.</p>
-                            <h4>Fourth level</h4>
-                            <p>Nulla efficitur eleifend nisi, sit amet bibendum sapien fringilla ac. Mauris euismod metus a tellus laoreet, at elementum ex efficitur.</p>
-                            <pre>
-                                &lt;!DOCTYPE html&gt;
-                                &lt;html&gt;
-                                &lt;head&gt;
-                                &lt;title&gt;Hello World&lt;/title&gt;
-                                &lt;/head&gt;
-                                &lt;body&gt;
-                                &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra nec nulla vitae mollis.&lt;/p&gt;
-                                &lt;/body&gt;
-                                &lt;/html&gt;
-                            </pre>
-                            <p>Maecenas eleifend sollicitudin dui, faucibus sollicitudin augue cursus non. Ut finibus eleifend arcu ut vehicula. Mauris eu est maximus est porta condimentum in eu justo. Nulla id iaculis sapien.</p>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>One</th>
-                                        <th>Two</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Three</td>
-                                        <td>Four</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Five</td>
-                                        <td>Six</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Seven</td>
-                                        <td>Eight</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Nine</td>
-                                        <td>Ten</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Eleven</td>
-                                        <td>Twelve</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <p>Phasellus porttitor enim id metus volutpat ultricies. Ut nisi nunc, blandit sed dapibus at, vestibulum in felis. Etiam iaculis lorem ac nibh bibendum rhoncus. Nam interdum efficitur ligula sit amet ullamcorper. Etiam tristique, leo vitae porta faucibus, mi lacus laoreet metus, at cursus leo est vel tellus. Sed ac posuere est. Nunc ultricies nunc neque, vitae ultricies ex sodales quis. Aliquam eu nibh in libero accumsan pulvinar. Nullam nec nisl placerat, pretium metus vel, euismod ipsum. Proin tempor cursus nisl vel condimentum. Nam pharetra varius metus non pellentesque.</p>
-                            <h5>Fifth level</h5>
-                            <p>Aliquam sagittis rhoncus vulputate. Cras non luctus sem, sed tincidunt ligula. Vestibulum at nunc elit. Praesent aliquet ligula mi, in luctus elit volutpat porta. Phasellus molestie diam vel nisi sodales, a eleifend augue laoreet. Sed nec eleifend justo. Nam et sollicitudin odio.</p>
                         </div>
                     </div>
                 </section>
