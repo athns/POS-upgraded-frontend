a04eab89f7b6989186f52cd7f5cc724383f330d3
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2022-01-09 23:05:53 -0600

    just doing some basic design.. added and array and starting a function

--- src/components/Options.js
+++ src/components/Options.js
@@ -10,27 +10,16 @@ class Options extends Component {
     }
     render() {
         return (
-            <body>
+            <div className='optionsBody'>
+                <span className='sideBarRight'></span>
+                <span className='sideBarLeft'></span>
                 <div className='NavBar'>
                     <h1 className='chosenItem'> Menu Item Chosen </h1>
                 </div>
-                <span className='sideBarRight'></span>
-                <span className='sideBarLeft'></span>
                 <div className='optionsContainer'>
-                    <div className='menuOption'>
-                        <h3>Option 1</h3>
-                        <ul className='center'>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                            <li className='center'><button type='radio'>Text</button></li>
-                        </ul>
+                    <form>
                         <div className='menuOption'>
-                            <h3>Option 2</h3>
+                            <h3>Option 1</h3>
                             <ul className='center'>
                                 <li className='center'><button type='radio'>Text</button></li>
                                 <li className='center'><button type='radio'>Text</button></li>
@@ -42,7 +31,7 @@ class Options extends Component {
                                 <li className='center'><button type='radio'>Text</button></li>
                             </ul>
                             <div className='menuOption'>
-                                <h3>Option 3</h3>
+                                <h3>Option 2</h3>
                                 <ul className='center'>
                                     <li className='center'><button type='radio'>Text</button></li>
                                     <li className='center'><button type='radio'>Text</button></li>
@@ -53,8 +42,8 @@ class Options extends Component {
                                     <li className='center'><button type='radio'>Text</button></li>
                                     <li className='center'><button type='radio'>Text</button></li>
                                 </ul>
-                                <h3>Option 4</h3>
                                 <div className='menuOption'>
+                                    <h3>Option 3</h3>
                                     <ul className='center'>
                                         <li className='center'><button type='radio'>Text</button></li>
                                         <li className='center'><button type='radio'>Text</button></li>
@@ -66,7 +55,7 @@ class Options extends Component {
                                         <li className='center'><button type='radio'>Text</button></li>
                                     </ul>
                                     <div className='menuOption'>
-                                        <h3>Option 5</h3>
+                                    <h3>Option 4</h3>
                                         <ul className='center'>
                                             <li className='center'><button type='radio'>Text</button></li>
                                             <li className='center'><button type='radio'>Text</button></li>
@@ -78,7 +67,7 @@ class Options extends Component {
                                             <li className='center'><button type='radio'>Text</button></li>
                                         </ul>
                                         <div className='menuOption'>
-                                            <h3>Option 6</h3>
+                                            <h3>Option 5</h3>
                                             <ul className='center'>
                                                 <li className='center'><button type='radio'>Text</button></li>
                                                 <li className='center'><button type='radio'>Text</button></li>
@@ -89,21 +78,43 @@ class Options extends Component {
                                                 <li className='center'><button type='radio'>Text</button></li>
                                                 <li className='center'><button type='radio'>Text</button></li>
                                             </ul>
+                                            <div className='menuOption'>
+                                                <h3>Option 6</h3>
+                                                <ul className='center'>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                    <li className='center'><button type='radio'>Text</button></li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-
+                        <a href='http://localhost:3000/MainMenu' className='buttonClass'>
+                            <button type='submit'>Place Your Order!</button>
+                        </a>
+                        <a href='http://localhost:3000/MainMen' className='buttonClass'>
+                            <button type='submit'>Order More!</button>
+                        </a>
+                    </form>
                 </div>
 
 
-            </body>
+
+            </div>
 
         )
     }
 }
 
 export default Options;
+
+function placeOrder() {
+
+}
