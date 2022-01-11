fbcec624ec83dfffaa019e38b73985377873d879
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2022-01-10 23:40:29 -0600

    re worked NAV, Finishing CSS touch ups on Components

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,8 +59,8 @@ class Login extends Component {
         <section id="login" className="hero is-fullheight">
           <div className="hero-body has-text-centered">
             <div className="login">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvZc2OTcmowp6dUbR_mwc_HPU5spYnvIwFSKyNdb1H1WrJUpEWJlDu-4q9rvgy7IrMfCc&usqp=CAU" width="325px" alt="logo"/>
-              <form onSubmit={this.handleSubmit.bind(this)}>
+              <img href='/Users/huntermcguire/Desktop/SEI-1025/UNIT-3/project/POS-upgraded-frontend/src/img/CoffeeBetch.png'></img>
+             <form onSubmit={this.handleSubmit.bind(this)}>
                 <div className="field">
                   <div className="control">
                     <input
--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -78,37 +78,29 @@ class MenuPage extends Component {
                 <nav className="navbar no-hover" role="navigation" aria-label="main navigation">
                     <div id="navbarBasicExample" className="navbar-menu">
                         <div className="navbar-item navbar-center">
-                            <h1 className='res-name'>Rising Bistro</h1>
+                            <h1 className='res-name'> The Coffee Stop</h1>
                         </div>
                         <div className="navbar-end">
-                            <div className="navbar-item">
-                                <div className="order-buttons">
-                                    <p className="seat-number">
-                                        <span>
-                                            Seat Number :
-                                        </span>
-                                    </p>
-                                    <p className="order-total">
-                                        <span>
+                            <div className=" totalDiv navbar-item">
+                                <div className="totalDiv order-buttons">
+                                        <span className='totalDiv'>
                                             Order Total : $
                                         </span>
-                                    </p>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </nav>
-                <section className="main-image">
-                    <div className="hero-body"></div>
-                </section>
+                
                 <script src="../js/bulma.js"></script>
                 <script src="../js/tabs.js"></script>
                 <section className="hero is-info">
 
                     {displayCharacters}
                 </section>
-                {<script src="../js/bulma.js"></script>}
-                {<script src="../js/tabs.js"></script>}
+                {/* {<script src="../js/bulma.js"></script>}
+                {<script src="../js/tabs.js"></script>} */}
 
             </>
         );
